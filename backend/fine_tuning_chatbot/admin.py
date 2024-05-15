2cfa9f677961ce52d910f2c6b59f082c2bf1986d
uuuu13 <a51903153@gmail.com>
2024-05-15 22:04:41 +0900

    model 생성

--- backend/fine_tuning_chatbot/admin.py
+++ backend/fine_tuning_chatbot/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import FineTunedModel, TrainingData
 
 # Register your models here.
+
+@admin.register(FineTunedModel)
+class FineTunedModelAdmin(admin.ModelAdmin):
+    list_display = ('model_name', 'base_name')
+    search_fields = ('model_name', 'base_name')
+
+@admin.register(TrainingData)
+class TrainingDataAdmin(admin.ModelAdmin):
+    list_display = ('prompt', 'completion', 'fine_tuned_model')
+    search_fields = ('prompt', 'completion', 'fine_tuned_model__model_name')
+    list_filter = ('fine_tuned_model',)
--- backend/fine_tuning_chatbot/models.py
+++ backend/fine_tuning_chatbot/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class FineTunedModel(models.Model): # 클래스 명이 DB에 담길 테이블 명이 됨
+    MODEL_CHOICES = [
+        ('ada', 'Ada'),
+        ('babbage', 'Babbage'),
+        ('curie', 'Curie'),
+        ('davinci', 'Davinci'),
+    ]
+
+    model_name = models.CharField(max_length=100)                       # 필드명이 테이블안의 컬럼명이 됨
+    base_name = models.CharField(max_length=100, choices=MODEL_CHOICES) # 필드명이 테이블안의 컬럼명이 됨
+
+    def __str__(self):
+        return self.model_name
+
+class TrainingData(models.Model):
+    fine_tuned_model = models.ForeignKey(FineTunedModel, on_delete=models.CASCADE, related_name='training_data')
+    # relate_name은 FineTuneModel 객체에서 TrainingData 객체에 접근할 때 사용할 역참조 이름을 정의
+
+    prompt = models.TextField()
+    completion = models.TextField()
+
+    def __str__(self):
+        return f"{self.fine_tuned_model.model_name}의 훈련 데이터"
