a74596cc6cf3a8d685c86160d9da5abb02dd3d02
uuuu13 <a51903153@gmail.com>
2024-05-27 22:35:22 +0900

    views 추가

--- backend/fine_tuning_chatbot/urls.py
+++ backend/fine_tuning_chatbot/urls.py
@@ -4,6 +4,10 @@ from . import views
 from .views import (
     FineTunedModelViewSet,
     TrainingDataViewSet,
+    convert_jsonl_file,
+    upload_jsonl_file,
+    create_finetune,
+    retrieve_finetune,
 )
 
 router = DefaultRouter()    # DefaultRouter의 인스턴스를 생성, DRF 라우터 클래스 중 하나로, 뷰셋을 자동으로 URL 패턴에 매핑해줌
@@ -12,7 +16,16 @@ router.register(r'fine_tuned_models', FineTunedModelViewSet)
 # TrainingDataViewSet 뷰셋을 training_data 경로에 등록함, training_data/ 경로에 대한 CRUD 엔드포인트가 자동으로 생성됨
 router.register(r'training_data', TrainingDataViewSet)
 
+openai_patterns = [
+    path('convert/<int:finetuned_model_id>/', convert_jsonl_file, name='convert_jsonl_file'),
+    path('upload/<int:finetuned_model_id>/', upload_jsonl_file, name='upload_jsonl_file'),
+    path('create/<int:finetuned_model_id>/', create_finetune, name='create_finetune'),
+    path('retrieve/<int:finetuned_model_id>/', retrieve_finetune, name='retrieve_finetune'),
+]
+
+# api/ ~
 urlpatterns = [
     path('hello/', views.hello_world),
     path('', include(router.urls)), # ''경로에 대해 router.urls를 포함함, 현재는 fine_tuned_models/ 경로와 training_data/ 경로
+    path('openai/', include((openai_patterns, 'openai'))),
 ]
--- backend/fine_tuning_chatbot/views.py
+++ backend/fine_tuning_chatbot/views.py
@@ -1,6 +1,13 @@
+import json
+import os
+
+import openai
+from django.http import JsonResponse
 from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework.decorators import api_view
+from django.conf import settings
+from django.core.files.storage import default_storage
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -24,3 +31,120 @@ class TrainingDataViewSet(viewsets.ModelViewSet):   # TrainingData 모델에 대
     permission_classes = (IsAuthenticated,)         # 인증된 사용자만 접근 가능
     queryset = TrainingData.objects.all()           # TrainingData 모델의 모든 객체를 가져옴
     serializer_class = TrainingDataSerializer       # 뷰셋에서 사용할 Serializer 클래스를 정의
+
+# fintuned_model_id를 이용하여 TrainingData 객체를 검색하고 JSON 형식으로 파일 저장
+def create_and_save_jsonl(finetuned_model_id):  # 인자는 finetuned_model_id를 받는다
+    training_data = TrainingData.objects.filter(fine_tuned_model_id=finetuned_model_id) # TrainingData 모델에서 fine_tuned_model_id가 일치하는 모든 객체를 필터링하고 변수에 저장함
+
+    file_name = f"fine_tuned_model_{finetuned_model_id}.jsonl" # 파일 이름 생성
+    with open(file_name, 'w') as f: # w 모드로 해당 파일들을 연다
+        for data in training_data:
+            f.write(json.dumps({'prompt': data.prompt + '\n', 'completion': data.completion + '\n'}))
+            f.write('\n')
+
+        return file_name, training_data
+
+
+# JSON Lines 형식으로 변환한 파일을 생성하고 API뷰에 반환
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def convert_jsonl_file(request, finetuned_model_id):
+    try:
+        FineTunedModel.objects.get(id=finetuned_model_id) # finetuned_model_id가 존재하지 않으면 FineTunedModel.DoesNotExsist 예외 발생
+
+        file_name, training_data = create_and_save_jsonl(finetuned_model_id)
+
+        file_info = os.stat(file_name) # 생성된 파일의 정보를 가져옴
+
+        # 응답으로 보낼 JSON 객체 생성
+        response = {
+            'file_name' : file_name,
+            'lines' : len(training_data),
+            'file_size' : file_info.st_size,
+        }
+        return JsonResponse(response)
+
+    except FineTunedModel.DoesNotExist:
+        return JsonResponse({'error': 'FineTunedModel with the given id does not exist.'}, status=404)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def upload_jsonl_file(request, finetuned_model_id):
+    try:
+        finetuned_model = FineTunedModel.objects.get(id=finetuned_model_id)
+
+        file_name, _ = create_and_save_jsonl(finetuned_model_id)
+
+        with open(file_name, 'rb') as file:
+            openai.api_key = settings.OPENAI_API_KYE
+            result = openai.File.create(file=file, purpose='fine-tune')
+
+        finetuned_model.file_id = result['id']
+        finetuned_model.save()
+
+        if default_storage.exsist(file_name):
+            default_storage.delete(file_name)
+
+        return JsonResponse(result)
+
+    except FineTunedModel.DoesNotExist:
+        return JsonResponse({'error': 'FineTunedModel with the given id does not exist.'}, status=404)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_finetune(request, finetuned_model_id):
+    try:
+        finetuned_model = FineTunedModel.objects.get(id=finetuned_model_id)
+
+        openai.api_key = settings.OPEN_API_KEY
+        result = openai.FineTune.create(
+            model = finetuned_model.base_model,
+            training_file = finetuned_model.file_id,
+        )
+
+        finetuned_model.fine_tune_id = result['id']
+        finetuned_model.save()
+
+        return JsonResponse(result)
+
+    except FineTunedModel.DoesNotExist:
+        return JsonResponse({'error': 'FineTunedModel with the given id does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def retrieve_finetune(request, finetuned_model_id):
+    try:
+        finetuned_model = FineTunedModel.objects.get(id=finetuned_model_id)
+
+        openai.api_key = settings.OPEN_API_KEY
+        fine_tune_id = finetuned_model.fine_tune_id
+
+        result = openai.FineTune.retrieve(id=fine_tune_id)
+
+        finetuned_model.status = result['status']
+        finetuned_model.fine_tuned_model = result['fine_tuned_model']
+        finetuned_model.save()
+
+        return JsonResponse(result)
+
+    except FineTunedModel.DoesNotExist:
+        return JsonResponse({'error': 'FineTunedModel with the given id does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
+
+
+
+
+
+
